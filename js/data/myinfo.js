ed2d8cdd55482ad485c904ead7ac1455acc87752
Jonathan Lam <jonlam@gmail.com>
2015-12-06 17:15:55 -0600

    Saving incompelte work. Beginning actual implemenation of portfolio/about me site.

---
+++ js/data/myinfo.js
@@ -0,0 +1,107 @@
+var workprojects = {
+
+    "projects": [
+        {
+            "type": "work",
+            "title": "HP Touchpoint Manager",
+            "dates": "January 2014 - current",
+            "description": "Cloud-based Manageability solution. Worked on various client and server side components.",
+            "images": [
+                "images/hptpm1.jpg",
+                "images/hptpm2.png"
+            ],
+            "url": "https://www.hptouchpointmanager.com"
+        },
+        {
+            "type": "work",
+            "title": "HP Client Integration Kit for Microsoft SCCM",
+            "dates": "2013",
+            "description": "Plugin for Microsoft's System Center Configuration Manager to enhance bare-metal image deployment for HP computers.",
+            "images": [
+                "images/sccm1.png"
+            ],
+            "url": "http://www.hp.com/go/clientmanagement"
+        }
+    ],
+
+    "moreinfo": {
+        "url": "link to linked in or somewhere else?"
+    }
+};
+
+// i won't list all here; i will add link to see more
+
+var fenanoProjects = {
+    "projects": [
+        {
+            "type": "fenano",
+            "title": "Project 1 - Layout and formatting",
+            "dates": "2015",
+            "description": "Basic exercise to construct a webpage to match a mockup",
+            "url": "link to live version",
+            "repo": "link to repo",
+
+        },
+        {
+            "type": "fenano",
+            "title": "Project 2 - Online Resume",
+            "dates": "2015",
+            "description": "Introduction to Javascript, CSS, and DOM manipulation (JQuery). Involved created resulable functions and data sources to create an online resume.",
+            "url": "link to live version",
+            "repo": "link to repo",
+        },
+        {
+            "type": "fenano",
+            "title": "Project 3 - Game",
+            "dates": "2015",
+            "description": "Create a frogger-type game, leveraging provided game engine and assets. Emphasized Javascript coding skills (classes, objects, etc.)",
+            "images": [
+                "images/fenano/project3.png"
+            ],
+            "url": "link to live version",
+            "repo": "link to repo",
+            "highlight": true
+        },
+        {
+            "type": "fenano",
+            "title": "Project 4 - Web Site Optimization",
+            "dates": "2015",
+            "description": "Optimize the loading and performance of various pages, using a variety of techniques and tools",
+            "images": [
+                "images/fenano/project4.png"
+            ],
+            "url": "link to live version",
+            "repo": "link to repo"
+        },
+        {
+            "type": "fenano",
+            "title": "Project 5 - Web App",
+            "dates": "2015",
+            "description": "Leverage 3rd party data sources and library to construct a Google Maps-based webapp",
+            "images": [
+                "images/fenano/project5-1.png",
+                "images/fenano/project5-2.png"
+            ],
+            "url": "link to live version",
+            "repo": "link to repo",
+            "highlight": true
+        },
+        {
+            "type": "fenano",
+            "title": "Project 6 - Unit tests",
+            "dates": "2015",
+            "description": "Using Jasmine, build up a suite of specs and tests against the provided web application.",
+            "images": [
+                "images/fenano/project6-1.png",
+            ],
+            "url": "link to live version",
+            "repo": "link to repo",
+            "highlight": true
+        },
+
+    ],
+
+    "moreinfo": {
+        "url": "link to github or udacity profile"
+    }
+};
---
+++ js/viewmodels/aboutme.js
@@ -0,0 +1,9 @@
+var AboutMeViewModel = function() {
+    // load in projects
+    // first direct load, later on load some other way...
+    var projects = 
+    this.projects =
+
+};
+
+
