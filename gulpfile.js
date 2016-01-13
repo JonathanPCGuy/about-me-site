f3e8808292e4734327be6c62fb62d17c3c8265a8
Jonathan Lam <jonlam@gmail.com>
2016-01-12 22:55:41 -0600

    refact, cleanup: Added gitignore for node_modules and dist dir Completed work on gulp build script

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,48 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var htmlreplace = require('gulp-html-replace');
+var runSequence = require('run-sequence');
 
+// todo: optimize css
 
-// first let's just get concat working
-// since that seems to be the biggest thing
+gulp.task('copy-css', function() {
+  return gulp.src(['./css/bootstrap.min.css', './css/style.css'])
+    .pipe(gulp.dest('./dist/css'));
+});
 
-// first just get the files combined
+// todo: optimize images
+
+gulp.task('copy-images', function() {
+  return gulp.src(['./images/**/*.*'])
+    .pipe(gulp.dest('./dist/images'));
+});
+
+gulp.task('copy-js-libs', function() {
+    return gulp.src(['./js/libs/**/*.*'])
+    .pipe(gulp.dest('./dist/js/libs'));
+});
+
+// todo: minify js, if we want to?
+
 gulp.task('concat-js', function() {
     return gulp.src(['./js/data/*.js', './js/viewmodels/*.js','./js/*.js'])
         .pipe(concat('all.js'))
         .pipe(gulp.dest('./dist/js/'));
 });
 
-// next modify html to read release version
+gulp.task('replace-js', function() {
+    return gulp.src('index.html')
+        .pipe(htmlreplace({
+            'js':'js/all.js'
+        }))
+        .pipe(gulp.dest('./dist/'));
+});
+
+// callback function/param is for async tasks
+// need to study that later
+gulp.task('build-site', function() {
+    // actually all of these can be parallel
+    // but for learning purposes i won't make them parallel
+    runSequence(['copy-css', 'copy-images', 'copy-js-libs' ],
+        ['concat-js', 'replace-js']);
+});
