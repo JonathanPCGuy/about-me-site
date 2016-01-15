928c12862add081bba61c1562186ee7aee4682d1
Jonathan Lam <jonlam@gmail.com>
2016-01-15 12:18:39 -0600

    bug: fixed incorrect e-mail address

--- js/data/contact.js
+++ js/data/contact.js
@@ -5,6 +5,6 @@ jlamdata.contact = {
         { 'title': 'LinkedIn', 'logo': 'images/contact/In-2C-66px-TM.png', 'url': 'https://www.linkedin.com/in/jonathan-lam-2332064' },
         { 'title': 'GitHub', 'logo': 'images/contact/GitHub-Mark-64px.png', 'url': 'https://www.github.com/JonathanPCGuy' },
         { 'title': 'Twitter', 'logo': 'images/contact/TwitterLogo_color.png', 'url': 'https://www.twitter.com/JonathanPCGuy' },
-        { 'title': 'E-mail', 'logo': 'images/simpleicon/mail59.svg', 'url': 'mailto:jonathanpcguy@gmail.com' }
+        { 'title': 'E-mail', 'logo': 'images/simpleicon/mail59.svg', 'url': 'mailto:jlampcguy@gmail.com' }
     ]
 };
